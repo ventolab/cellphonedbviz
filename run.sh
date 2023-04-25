61af5e1c2cc4f52946a7dd31d1f48f1ff46e79ab
Robert Petryszak <info@datasome.co.uk>
2023-04-25 09:37:45 +0100

    Changed port to 8001 to avoid conflict on cellgeni

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-uvicorn main:app --host=0.0.0.0 --port=${APP_PORT:-8000} --reload --log-level=debug
+uvicorn main:app --host=0.0.0.0 --port=${APP_PORT:-8001} --reload --log-level=debug
