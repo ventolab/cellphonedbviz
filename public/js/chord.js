13fcaeb1b796fee64438d44b787d64bb5a2348ee
Robert Petryszak <info@datasome.co.uk>
2023-07-17 16:32:18 +0100

    Encoded number of interactions as heatmap colour into chord plots - previously the colour was assigned based on cell type name

--- public/js/chord.js
+++ public/js/chord.js
@@ -1,11 +1,11 @@
 import {Runtime, Library, Inspector} from "../external/js/runtime.unminified.noparamsdisplay.js";
 
-function _chart(d3,width,height,chord,matrix,DOM,outerRadius,ribbon,color,names,formatValue,arc,title)
+function _chart(d3,width,height,chord,matrix,DOM,outerRadius,ribbon,color,names,formatValue,arc,title,maxNumInts)
 {
     const svg = d3.create("svg")
-        .attr("width", width*1.5)
-        .attr("height", height*1.5)
-        .attr("viewBox", [-width / 2, -height / 2, width + 50, height + 50]);
+        .attr("width", width*1.6)
+        .attr("height", height*1.6)
+        .attr("viewBox", [-width / 1.6, -height / 1.6, width + 50, height + 50]);
 
     // Insert title
     svg.append("text")
@@ -30,7 +30,7 @@ function _chart(d3,width,height,chord,matrix,DOM,outerRadius,ribbon,color,names,
     .data(chords)
     .join("path")
         .attr("d", ribbon)
-        .attr("fill", d => color(names[d.target.index]))
+        .attr("fill", d => color(d.target.value))
         .style("mix-blend-mode", "multiply")
     // The following acts as a tooltip over a ribbon
     .append("title")
@@ -53,7 +53,8 @@ function _chart(d3,width,height,chord,matrix,DOM,outerRadius,ribbon,color,names,
     .join("g")
         .call(g => g.append("path")
         .attr("d", arc)
-        .attr("fill", d => color(names[d.index]))
+        // Set the colour of the outer ring to light grey
+        .attr("fill", d => "#B9BBB6")
         .attr("stroke", "#fff"))
         .call(
     g => g.append("text")
@@ -73,6 +74,96 @@ function _chart(d3,width,height,chord,matrix,DOM,outerRadius,ribbon,color,names,
         .text(d => `${names[d.index]}
 ${formatValue(d3.sum(matrix[d.index]))} outgoing interactions
 ${formatValue(d3.sum(matrix, row => row[d.index]))} incoming interactions`));
+
+  // Display colour legend bar
+    var min_ints = 0;
+    var max_ints = parseInt(maxNumInts);
+    const colorscale = d3
+      .scaleSequential()
+      .domain([max_ints, min_ints])
+      // See: https://observablehq.com/@d3/sequential-scales
+      .interpolator(d3.interpolateRdYlBu)
+
+    var legend_width=30
+    legend_height=100,
+    legend_xPos= -width/1.7,
+    title_xPos = legend_xPos * 0.5,
+    legend_yPos=-45;
+
+  // Colour legend:
+  // See: https://blog.scottlogic.com/2019/03/13/how-to-create-a-continuous-colour-range-legend-using-d3-and-d3fc.html
+  // Band scale for x-axis
+
+  const domain=[min_ints, max_ints]
+  const legend_xScale = d3
+    .scaleBand()
+    .domain([0, 1])
+    .range([0, legend_width]);
+
+  // Linear scale for y-axis
+  const legend_yScale = d3
+    .scaleLinear()
+    .domain(domain)
+    .range([legend_height, 0]);
+
+  // An array interpolated over our domain where height is the height of the bar
+  // Padding the domain by 3%
+  // This will have an effect of the bar being 3% longer than the axis label
+  // (otherwise top/bottom figures on the legend axis would be cut in half)
+  const paddedDomain = fc.extentLinear()
+    .pad([0.03, 0.03])
+    .padUnit("percent")(domain);
+  [min_ints, max_ints] = paddedDomain;
+  const expandedDomain = d3.range(min_ints, max_ints, (max_ints - min_ints) / legend_height);
+
+  // Define the colour legend bar
+  const svgBar = fc
+    .autoBandwidth(fc.seriesSvgBar())
+    .xScale(legend_xScale)
+    .yScale(legend_yScale)
+    .crossValue(0)
+    .baseValue((_, i) => (i > 0 ? expandedDomain[i - 1] : 0))
+    .mainValue(d => d)
+    .decorate(selection => {
+      selection.selectAll("path").style("fill", d => colorscale(d));
+    });
+
+    // Add the colour legend header
+    svg
+    .append("text").attr("x", legend_xPos - 7).attr("y", -65).text("Number of").style("font-size", "10px")
+    .append('tspan').attr("x", legend_xPos - 7).attr("y", -55).text("interactions")
+    .attr("alignment-baseline","middle");
+
+  // Draw the legend bar
+  const colourLegendBar = svg
+    .append("g")
+    .attr("transform", function() {
+        return "translate(" + legend_xPos + "," + legend_yPos + ")";
+      })
+    .datum(expandedDomain)
+    .call(svgBar);
+
+  // Linear scale for legend label
+  const legendLabel_yScale = d3
+    .scaleLinear()
+    .domain(paddedDomain)
+    .range([legend_height, 0]);
+
+  // Defining our label
+  const axisLabel = fc
+    .axisRight(legendLabel_yScale)
+    .tickValues([...domain, (domain[1] + domain[0]) / 2])
+    .tickSizeOuter(0);
+
+  // Drawing and translating the label
+  colourLegendBar.append("g")
+    // .attr("transform", `translate(${barWidth})`)
+    .attr("transform", "translate(" + 15 + ")")
+    .datum(expandedDomain)
+    .call(axisLabel)
+    .select(".domain")
+    .attr("visibility", "hidden");
+  // Display colour legend bar - end
 
     return svg.node();
 }
@@ -109,29 +200,16 @@ d3.ribbon()
     .padAngle(1 / innerRadius)
 )}
 
-// https://stackoverflow.com/questions/34163662/is-there-a-way-to-generate-more-than-20-colors-in-d3
-var d3_category20 = [
-    0x1f77b4, 0xaec7e8,
-    0xff7f0e, 0xffbb78,
-    0x2ca02c, 0x98df8a,
-    0xd62728, 0xff9896,
-    0x9467bd, 0xc5b0d5,
-    0x8c564b, 0xc49c94,
-    0xe377c2, 0xf7b6d2,
-    0x7f7f7f, 0xc7c7c7,
-    0xbcbd22, 0xdbdb8d,
-    0x17becf, 0x9edae5
-    ].map(d3_rgbString);
-    
 function d3_rgbString (value) {
     return d3.rgb(value >> 16, value >> 8 & 0xff, value & 0xff);
 }
 
-function _color(d3,names){return(
-    // d3.scaleOrdinal(names, d3.schemeCategory10)
-    // d3.scaleOrdinal(names, d3.quantize(d3.interpolateRainbow, names.length))
-    d3.scaleOrdinal(names, d3_category20)
-
+function _color(d3,data){
+  const max_ints = Math.max(...Array.from(new Set(data.flatMap(d => [d.value]))));
+  return(d3
+  .scaleSequential()
+  .domain([max_ints, 0])
+  .interpolator(d3.interpolateRdYlBu)
 )}
 
 function _formatValue(){return(
@@ -159,8 +237,8 @@ function _d3(require){return(
 require("d3@6")
 )}
 
-function define(main, observer, data, title, plotCnt) {
-     main.variable(observer("chart")).define("chart", ["d3","width","height","chord","matrix","DOM","outerRadius","ribbon","color","names","formatValue","arc","title"], _chart);
+function define(main, observer, data, title, plotCnt, max_num_ints) {
+     main.variable(observer("chart")).define("chart", ["d3","width","height","chord","matrix","DOM","outerRadius","ribbon","color","names","formatValue","arc","title","maxNumInts"], _chart);
      main.variable(observer("data")).define("data", [], data);
      main.variable(observer("title")).define("title", [], title);
      main.variable(observer("names")).define("names", ["data"], _names);
@@ -168,7 +246,7 @@ function define(main, observer, data, title, plotCnt) {
      main.variable(observer("chord")).define("chord", ["d3","innerRadius"], _chord);
      main.variable(observer("arc")).define("arc", ["d3","innerRadius","outerRadius"], _arc);
      main.variable(observer("ribbon")).define("ribbon", ["d3","innerRadius"], _ribbon);
-     main.variable(observer("color")).define("color", ["d3","names"], _color);
+     main.variable(observer("color")).define("color", ["d3","data"], _color);
      main.variable(observer("formatValue")).define("formatValue", _formatValue);
      // all cell types default
      var innerRadius = 200;
@@ -184,10 +262,11 @@ function define(main, observer, data, title, plotCnt) {
      main.variable(observer("innerRadius")).define("innerRadius", ["width","height"], innerRadius);
      main.variable(observer("width")).define("width", size);
      main.variable(observer("height")).define("height", ["width"], size);
+     main.variable(observer("maxNumInts")).define("maxNumInts", [], max_num_ints);
      main.variable(observer("d3")).define("d3", ["require"], _d3);
  }
 
- export default function generateCellCellInteractionSummaryChordPlot(data, cellTypes, title, plotCnt) {
+ export default function generateCellCellInteractionSummaryChordPlot(data, cellTypes, title, plotCnt, max_num_ints) {
     const num_ints_csv = filterNumInteractions(data, cellTypes, false);
     // See: https://observablehq.com/@observablehq/advanced-embeds
     // See: https://observablehq.com/@observablehq/stdlib
@@ -200,5 +279,6 @@ function define(main, observer, data, title, plotCnt) {
     $(plotId).empty();
     const observer = Inspector.into(document.querySelector(plotId));
     const chordData = d3.csvParse(num_ints_csv, d3.autoType);
-    define(module, observer, chordData, title, plotCnt);
+    define(module, observer, chordData, title, plotCnt, max_num_ints);
+
  }
