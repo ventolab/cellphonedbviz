538bf400e1d3496ff8ea74ccf18b10928e58212a
Jessica Cox <jc52@sanger.ac.uk>
2024-12-19 11:24:06 +0000

    Fix error when using microenvironment on cell-cell communication search plot

--- public/js/cpdbviz.js
+++ public/js/cpdbviz.js
@@ -360,15 +360,7 @@ function enable_me2ct_select(microenvironment2cell_types, all_cell_types,
                $('.sge_selected_celltypes').hide();
                $('#sge_celltype_input').prop( "disabled", true );
                $('#sge_select_all_celltypes').addClass('disabled');
-            } /*else if (selected_microenvironments_div == 'cci_search_selected_microenvironments') {
-              // Disable cell type and cell type pair inputs as the requirement is for
-              // microenvironments, cell type and cell type pair inputs to be mutually exclusive
-              $('#cci_search_celltype_input').prop( "disabled", true );
-              $('.cci_search_selected_celltypes').hide();
-              $('.cci_search_selected_celltype_pairs').empty();
-              $('#cci_search_select_all_celltypes').addClass('disabled');
-              toggleDisableCellTypePairGrid(true);
-            }*/
+            }
         } else {
             if (selected_microenvironments_div == 'sge_selected_microenvironments') {
                selected_cell_types = all_cell_types;
@@ -386,7 +378,6 @@ function enable_me2ct_select(microenvironment2cell_types, all_cell_types,
               $('.cci_search_selected_celltypes').show();
               $('#sge_select_all_celltypes').prop( "disabled", false );
               $('#cci_search_select_all_celltypes').removeClass('disabled');
-              toggleDisableCellTypePairGrid(false);
             }
         }
     });
